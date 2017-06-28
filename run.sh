98593de2bec99d3ba1b2f4d8e9d54529bc1e9f34
endokai <my1130546419@gmail.com>
2017-06-28 22:24:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-#!/bin/sh -e
-cd /root/shadowsocksr-manyuser/shadowsocks && \
-echo "
+#!/bin/sh
+cd /root/shadowsocksr-manyuser/shadowsocks
+sed -i 's/'8388/"${ssrport}"'/g' user-config.json
+sed -i 's/'pass1234/"${pass}"'/g' user-config.json
+sed -i 's/'auth_aes128_md5/"${protocol}"'/g' user-config.json
+sed -i 's/'aes-128-ctr/"${method}"'/g' user-config.json
+sed -i 's/'tls1.2_ticket_auth/"${obfs}"'/g' user-config.json
+python server.py -d start
+kcptunserver -t "127.0.0.1:${ssrport}" -l ":${kcpport}" -mode fast2
