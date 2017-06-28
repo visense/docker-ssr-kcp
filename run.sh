18aed0c49e8ff2fd1b000cdb2869929c194bd819
endokai <my1130546419@gmail.com>
2017-06-28 22:25:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ sed -i 's/'pass1234/"${pass}"'/g' user-config.json
 sed -i 's/'auth_aes128_md5/"${protocol}"'/g' user-config.json
 sed -i 's/'aes-128-ctr/"${method}"'/g' user-config.json
 sed -i 's/'tls1.2_ticket_auth/"${obfs}"'/g' user-config.json
+cat user-config.json
 python server.py -d start
 kcptunserver -t "127.0.0.1:${ssrport}" -l ":${kcpport}" -mode fast2
